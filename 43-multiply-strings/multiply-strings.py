0acf6b5542093c1eb053e57abb05ba33278dd5e0
laasya7777 <kanchanasailaasya@gmail.com>
2025-08-03 19:15:09 +0530

    Time: 4 ms (54.77%) | Memory: 17.8 MB (69.10%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == '0' or num2 == '0':
+            return '0'
+
+        def s(num):  
+            ans = 0
+            for i in num:
+                ans = ans * 10 + (ord(i) - ord('0'))
+            return ans
+
+        def t(n):  
+            res = ''
+            while n:
+                a = n % 10
+                n = n // 10
+                res = chr(ord('0') + a) + res
+            return res
+
+        return t(s(num1) * s(num2))
