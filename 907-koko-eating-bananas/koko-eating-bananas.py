6acba4a02a87fb800c0184e0cd3db6652a339c43
laasya7777 <kanchanasailaasya@gmail.com>
2025-09-26 10:16:22 +0530

    Time: 139 ms (94.29%) | Memory: 19.3 MB (10.27%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,21 @@
+from math import ceil
+from typing import List
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low = 1
+        high = max(piles)
+
+        while low <= high:
+            mid = (low + high) // 2
+            hours = 0
+
+            for num in piles:
+                hours += ceil(num / mid)
+
+            if hours <= h:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
