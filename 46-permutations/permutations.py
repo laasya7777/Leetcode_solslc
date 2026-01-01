3b8bf0b7eac1db0690e8e1fece3919c2d8826d72
laasya7777 <kanchanasailaasya@gmail.com>
2026-01-01 14:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.83%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,16 +1,20 @@
 class Solution:
     def permute(self, nums):
         res = []
+        used = [False] * len(nums)
 
-        def backtrack(start):
-            if start == len(nums):
-                res.append(nums[:])
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
                 return
 
-            for i in range(start, len(nums)):
-                nums[start], nums[i] = nums[i], nums[start]
-                backtrack(start + 1)
-                nums[start], nums[i] = nums[i], nums[start]
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
 
-        backtrack(0)
+                used[i] = True
+                backtrack(path + [nums[i]])
+                used[i] = False
+
+        backtrack([])
         return res
