9b38d52091f7703dd8c39a0b0247eceb2a32f4e4
laasya7777 <kanchanasailaasya@gmail.com>
2026-01-13 18:45:55 +0530

    Time: 2770 ms (27.56%) | Memory: 48.5 MB (17.89%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.py
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -1,31 +1,25 @@
 class Solution:
     def separateSquares(self, squares):
-        def area_above(Y):
-            total = 0.0
-            for x, y, l in squares:
+        def diff(Y):
+            d = 0.0
+            for _, y, l in squares:
                 top = y + l
+                area = l * l
                 if Y <= y:
-                    total += l * l
-                elif Y < top:
-                    total += (top - Y) * l
-            return total
-
-        def area_below(Y):
-            total = 0.0
-            for x, y, l in squares:
-                top = y + l
-                if Y >= top:
-                    total += l * l
-                elif Y > y:
-                    total += (Y - y) * l
-            return total
+                    d += area
+                elif Y >= top:
+                    d -= area
+                else:
+                    d += (top - Y) * l
+                    d -= (Y - y) * l
+            return d
 
         low = min(y for _, y, _ in squares)
         high = max(y + l for _, y, l in squares)
 
-        for _ in range(60):  
+        for _ in range(50):   
             mid = (low + high) / 2
-            if area_above(mid) > area_below(mid):
+            if diff(mid) > 0:
                 low = mid
             else:
                 high = mid
