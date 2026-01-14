a29a5bb89711b77f4e32e8d67c125375c701456d
laasya7777 <kanchanasailaasya@gmail.com>
2026-01-14 21:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (10.16%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,14 +1,21 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
+        splitted = path.split('/')
+        dir_names = [name for name in splitted if name != '']
         stack = []
-
-        for part in path.split('/'):
-            if part == "" or part == ".":
+        for dir_name in dir_names:
+            if dir_name == '.':
                 continue
-            elif part == "..":
+            elif dir_name == '..':
                 if stack:
                     stack.pop()
             else:
-                stack.append(part)
-
-        return "/" + "/".join(stack)
+                stack.append(dir_name)
+        
+        if not stack:
+            return '/'
+        else:
+            result = ''
+            for name in stack:
+                result+='/'+name
+            return result
