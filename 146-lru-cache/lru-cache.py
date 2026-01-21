2f1d49cc608504351202dc929a4dd790b7655684
laasya7777 <kanchanasailaasya@gmail.com>
2026-01-21 17:57:03 +0530

    Time: 386 ms (6.58%) | Memory: 76.2 MB (99.70%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,19 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        val = self.cache.pop(key)
+        self.cache[key] = val      
+        return val               
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            lru = list(self.cache.keys())[0]
+            self.cache.pop(lru)
