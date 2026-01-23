a3eed63a5fd3aaa1211e99005ae0ad14994d379a
laasya7777 <kanchanasailaasya@gmail.com>
2026-01-23 18:36:51 +0530

    Time: 3409 ms (72.92%) | Memory: 19.7 MB (9.07%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,32 +1,21 @@
 class Solution:
-    def exist(self, board, word):
-        rows, cols = len(board), len(board[0])
-
-        def dfs(r, c, idx):
-           
-            if idx == len(word):
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows,cols=len(board),len(board[0])
+        def bt(r,c,idx):
+            if idx==len(word):
                 return True
-
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[idx]:
+            if r<0 or r>=rows or c<0 or c>=cols or word[idx]!=board[r][c]:
                 return False
-
-            temp = board[r][c]
-            board[r][c] = "#"
-
-            found = (
-                dfs(r + 1, c, idx + 1) or
-                dfs(r - 1, c, idx + 1) or
-                dfs(r, c + 1, idx + 1) or
-                dfs(r, c - 1, idx + 1)
-            )
-
-            board[r][c] = temp
-
+            temp=board[r][c]
+            board[r][c]="$"
+            found=(bt(r+1,c,idx+1) or
+                  bt(r-1,c,idx+1)or
+                  bt(r,c+1,idx+1) or
+                   bt(r,c-1,idx+1))
+            board[r][c]=temp
             return found
-
         for i in range(rows):
             for j in range(cols):
-                if board[i][j] == word[0] and dfs(i, j, 0):
+                if bt(i,j,0):
                     return True
-
         return False
