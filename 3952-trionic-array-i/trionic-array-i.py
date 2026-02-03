ef80dd899114769e38f21e2d4c5668d0d66c7e3e
laasya7777 <kanchanasailaasya@gmail.com>
2026-02-03 18:28:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (19.11%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isTrionic(self, nums):
+        n = len(nums)
+        if n < 4:
+            return False
+
+        i = 1
+
+        while i < n and nums[i] > nums[i - 1]:
+            i += 1
+        if i == 1 or i == n:
+            return False
+
+        while i < n and nums[i] < nums[i - 1]:
+            i += 1
+        if i == n:
+            return False
+
+        while i < n and nums[i] > nums[i - 1]:
+            i += 1
+
+        return i == n
