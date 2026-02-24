73c32874b35971f52d9aa20f214c92c172eb4443
laasya7777 <kanchanasailaasya@gmail.com>
2026-02-24 17:59:58 +0530

    Time: 332 ms (76.89%) | Memory: 19.4 MB (69.99%) - LeetSync

--- 454-4sum-ii/4sum-ii.py
+++ 454-4sum-ii/4sum-ii.py
@@ -1,17 +1,11 @@
 class Solution:
     def fourSumCount(self, nums1, nums2, nums3, nums4):
-        from collections import defaultdict
-        
-        count = defaultdict(int)
-        
-        # Store sums of nums1 and nums2
+        from collections import defaultdict  
+        count = defaultdict(int)  
         for a in nums1:
             for b in nums2:
-                count[a + b] += 1
-        
+                count[a + b] += 1     
         result = 0
-        
-        # Check sums of nums3 and nums4
         for c in nums3:
             for d in nums4:
                 result += count[-(c + d)]
