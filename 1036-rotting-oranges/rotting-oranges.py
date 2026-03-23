8bdebcc8c5e8a6370a63700281ab41b13432b4cd
laasya7777 <kanchanasailaasya@gmail.com>
2026-03-23 20:02:20 +0530

    Time: 3 ms (76.09%) | Memory: 19.3 MB (65.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,39 +1,26 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        def bfs(row, col, rows, cols, grid, visited):
-            return
-
-        rows = len(grid)
-        cols = len(grid[0])
-        queue = deque()
-        minute = 0
+        res=0 
         fresh=0
-        visited = []
-        for i in range(rows):
-            lst = [0] * cols
-            visited.append(lst)
-
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == 2:
-                    queue.append((row, col, 0))
-                elif grid[row][col] == 1:
-                    fresh += 1
-
-        while queue:
-            row, col, minute = queue.popleft()
-            for ur, uc in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-                nr = row + ur
-                nc = col + uc
-                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
-                    grid[nr][nc] = 2
-                    fresh -= 1
-                    queue.append((nr, nc, minute+ 1))
-
-        if fresh > 0:
-            return -1
+        rows,cols=len(grid),len(grid[0])
+        q=[]
+        dirs=[(0,1),(0,-1),(1,0),(-1,0)]
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==2:
+                    q.append((r,c,0))
+                if grid[r][c]==1:
+                    fresh+=1
+        while q:
+            row,col,x=q.pop(0)
+            for dr,dc in dirs:
+                r,c=row+dr,col+dc
+                if 0<=r <rows and 0<=c<cols and grid[r][c]==1:
+                    grid[r][c]=2
+                    fresh-=1
+                    q.append((r,c,x+1))
+                    res=max(res,x+1)
+        if fresh==0:
+            return res
         else:
-            return minute
+            return -1
